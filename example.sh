ecb5fd8575d020e91a0af8a0fe9133675e2799b3
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-14 15:33:50 +0100

    update container

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@
 # generate dataset with certain seed
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "specific_example/template",
+  id = "specific_example/urd",
   num_cells = 300,
   num_features = 40,
   model = "bifurcating",
