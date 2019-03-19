007baca8cc8f33b70a90f835089302c3f95b546a
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-19 12:25:25 +0100

    update container

--- run.R
+++ run.R
@@ -22,18 +22,18 @@ library(URD)
 #   Load data                                                               ####
 
 count <- task$counts
-params <- task$params
+parameters <- task$parameters
 start_id <- task$priors$start_id
 
 #   ____________________________________________________________________________
 #   Infer trajectory                                                        ####
-if (params$sigma.use <= 0) {
-  params$sigma.use <- NULL
+if (parameters$sigma.use <= 0) {
+  parameters$sigma.use <- NULL
 }
-if (params$knn <= 0) {
-  params$knn <- destiny:::find_dm_k(nrow(count))
-  if (params$knn >= nrow(count)) {
-    params$knn <- round(log10(nrow(count)) * 10)
+if (parameters$knn <= 0) {
+  parameters$knn <- destiny:::find_dm_k(nrow(count))
+  if (parameters$knn >= nrow(count)) {
+    parameters$knn <- round(log10(nrow(count)) * 10)
   }
 }
 
@@ -46,16 +46,16 @@ checkpoints <- list(method_afterpreproc = as.numeric(Sys.time()))
 # Calculate Diffusion Map
 urd <- calcDM(
   urd,
-  knn = params$knn,
-  sigma.use = params$sigma.use,
-  distance = params$distance
+  knn = parameters$knn,
+  sigma.use = parameters$sigma.use,
+  distance = parameters$distance
 )
 
 # Then we run 'flood' simulations
 urd.floods <- floodPseudotime(
   urd,
   root.cells = start_id,
-  n = params$n_floods,
+  n = parameters$n_floods,
   minimum.cells.flooded = 0,
   verbose = FALSE
 )
@@ -65,29 +65,29 @@ urd <- floodPseudotimeProcess(
   urd,
   urd.floods,
   floods.name = "pseudotime",
-  stability.div = params$stability.div
+  stability.div = parameters$stability.div
 )
 
 # Calculate PCA and tSNE
 urd <- calcPCA(
   urd,
-  mp.factor = params$mp.factor
+  mp.factor = parameters$mp.factor
 )
 urd <- calcTsne(
   urd,
-  perplexity = params$perplexity,
-  theta = params$theta,
-  max_iter = params$max_iter
+  perplexity = parameters$perplexity,
+  theta = parameters$theta,
+  max_iter = parameters$max_iter
 )
 
 # Calculate graph clustering of these cells
 urd <- graphClustering(
   urd,
-  num.nn = params$num.nn,
-  do.jaccard = params$do.jaccard,
+  num.nn = parameters$num.nn,
+  do.jaccard = parameters$do.jaccard,
   method = "Louvain"
 )
-cluster_name <- paste0("Louvain-", params$num.nn)
+cluster_name <- paste0("Louvain-", parameters$num.nn)
 
 # Determine the parameters of the logistic used to bias the transition probabilities. The procedure
 # is relatively robust to this parameter, but the cell numbers may need to be modified for larger
@@ -95,8 +95,8 @@ cluster_name <- paste0("Louvain-", params$num.nn)
 urd.ptlogistic <- pseudotimeDetermineLogistic(
   urd,
   "pseudotime",
-  optimal.cells.forward = params$optimal.cells.forward,
-  max.cells.back = params$max.cells.back,
+  optimal.cells.forward = parameters$optimal.cells.forward,
+  max.cells.back = parameters$max.cells.back,
   do.plot = FALSE
 )
 
@@ -104,7 +104,7 @@ urd.ptlogistic <- pseudotimeDetermineLogistic(
 urd.biased.tm <- as.matrix(pseudotimeWeightTransitionMatrix(
   urd,
   "pseudotime",
-  logistic.params = urd.ptlogistic
+  logistic.parameters = urd.ptlogistic
 ))
 
 # Simulate the biased random walks from each tip
@@ -113,8 +113,8 @@ urd.walks <- simulateRandomWalksFromTips(
   tip.group.id = cluster_name,
   root.cells = start_id,
   transition.matrix = urd.biased.tm,
-  n.per.tip = params$n.per.tip,
-  root.visits = params$root.visits,
+  n.per.tip = parameters$n.per.tip,
+  root.visits = parameters$root.visits,
   max.steps = 5000,
   verbose = FALSE
 )
@@ -123,7 +123,7 @@ urd.walks <- simulateRandomWalksFromTips(
 urd <- processRandomWalksFromTips(
   urd,
   urd.walks,
-  n.subsample = params$n.subsample,
+  n.subsample = parameters$n.subsample,
   verbose = FALSE
 )
 
@@ -137,10 +137,10 @@ urd.tree <- buildTree(
   urd.tree,
   pseudotime = "pseudotime",
   tips.use = tips.use,
-  divergence.method = params$divergence.method,
-  cells.per.pseudotime.bin = params$cells.per.pseudotime.bin,
-  bins.per.pseudotime.window = params$bins.per.pseudotime.window,
-  p.thresh = params$p.thresh,
+  divergence.method = parameters$divergence.method,
+  cells.per.pseudotime.bin = parameters$cells.per.pseudotime.bin,
+  bins.per.pseudotime.window = parameters$bins.per.pseudotime.window,
+  p.thresh = parameters$p.thresh,
   dendro.cell.jitter = 0,
   dendro.cell.dist.to.tree = 0,
   save.all.breakpoint.info = TRUE
